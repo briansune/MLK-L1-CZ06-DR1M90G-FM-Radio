95aefaa903963275ce39e83d55d119ce0367bb80
briansune <briansune@gmail.com>
2024-12-03 18:49:43 +0800

    update

--- prj/ad9361_cmos_fm.sdc
+++
@@ -1,3 +0,0 @@
-create_clock -name sys_clk -period 10 -waveform {0 5} [get_ports {sys_clk}]
-create_clock -name data_clk -period 27.778 -waveform {0 13.889} [get_ports {data_clk_p}]
-derive_pll_clocks
--- prj/top/ip/top_Constant_0/constant_dbe528ceae18.v
+++
@@ -1,8 +0,0 @@
-
-module constant_dbe528ceae18 #(parameter WIDTH = 3, 
-        parameter OUT_MODE = 1) (
-    output [(WIDTH - 1):0] dout) ;
-    assign dout = (OUT_MODE ? {WIDTH{1'b1}} : 'b0) ; 
-endmodule
-
-
