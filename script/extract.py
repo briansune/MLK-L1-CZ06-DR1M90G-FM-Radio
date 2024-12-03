1eadd6e00d1bafa06747bf3f085078140157a1cb
briansune <briansune@gmail.com>
2024-12-03 17:05:05 +0800

    Revert "update"
    This reverts commit 986ab35fc8f69ad33fa6606c77cf0babcaf98837.

--- script/extract.py
+++
@@ -1,8 +0,0 @@
-import re
-
-# with open('default.txt', 'r') as f:
-with open('my_lut.txt', 'r') as f:
-    for line in f.readlines():
-        scan_line = re.search('ad9361_cmd_data[\s]=[\s]{(.*)};', line)
-        if scan_line:
-            print(scan_line.group(1).strip().replace('\t', ' '))
